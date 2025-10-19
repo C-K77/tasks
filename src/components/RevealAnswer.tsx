f846f01331a7aff88305396188593ae64f95d842
ckelley_17 <ckelleycek@gmail.com>
2025-10-19 17:30:21 -0400

    Added a passing reveal ans fx

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [vis, setVis] = useState<boolean>(false); //should it be shown Y/N
+
+    function toggleAnswer(): void {
+        setVis(!vis);
+    } //changes vis status
+
+    return (
+        //makes button linked to that fx and only works if its 42
+        <div>
+            <Button onClick={toggleAnswer}>Reveal Answer </Button>
+
+            {vis && <p>42</p>}
+        </div>
+    );
 }
